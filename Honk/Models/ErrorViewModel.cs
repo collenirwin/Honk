9fa16cad614775e6e53f176ee7ec349ef5bbb2bc
collenirwin <collenirwin@gmail.com>
2021-12-05 14:58:10 -0500

    code cleanup

--- Honk/Models/ErrorViewModel.cs
+++ Honk/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Honk.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Honk.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
