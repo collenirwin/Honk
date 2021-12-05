ff821990189ac2b04e8319fb8a1923ac321c1ad8
collenirwin <collenirwin@gmail.com>
2021-12-05 13:19:44 -0500

    add project start

---
+++ Honk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Honk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
