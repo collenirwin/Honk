74dbb6296986e2a1cd761cd8f302a59d524fa9b6
collenirwin <collenirwin@gmail.com>
2021-12-05 15:05:18 -0500

    add app user model and dbcontext

---
+++ Honk/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Honk.Models.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Honk.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    /// <inheritdoc />
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ Honk/Models/Data/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Honk.Models.Data;
+
+public class ApplicationUser : IdentityUser
+{
+}
