c01528a3a6822bb6d6cf3246010ad58fd1431729
collenirwin <collenirwin@gmail.com>
2022-11-12 22:22:49 -0500

    add register/login/profile pages + nav

---
+++ Honk/Client/Models/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Honk.Client.Models;
+
+public class ProfileViewModel
+{
+    public string? UserName { get; set; }
+
+    public string? Email { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+}
