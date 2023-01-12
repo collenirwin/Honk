747ed9f17529e78feaa84ee44ea3721e0aa3ba30
collenirwin <collenirwin@gmail.com>
2023-01-11 20:39:46 -0500

    add FriendRequestNotification model

---
+++ Honk/Server/Models/Data/FriendRequestNotification.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Honk.Server.Models.Data;
+
+/// <summary>
+/// Represents a friend request notification.
+/// </summary>
+public class FriendRequestNotification : BaseNotification
+{
+    /// <summary>
+    /// <see cref="ApplicationUser.Id"/> of the user who is requesting to be friends with the <see cref="For"/> user.
+    /// </summary>
+    [ForeignKey(nameof(Requestor))]
+    public string RequestorUserId { get; set; } = null!;
+
+    /// <summary>
+    /// The user who is requesting to be friends with the <see cref="For"/> user.
+    /// </summary>
+    public ApplicationUser? Requestor { get; set; }
+}
