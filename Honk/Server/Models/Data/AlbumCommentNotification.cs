082424a2eaca8568811b3ebd6ba00a4f7c10c939
collenirwin <collenirwin@gmail.com>
2023-01-11 20:51:55 -0500

    add comment notification models

---
+++ Honk/Server/Models/Data/AlbumCommentNotification.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Honk.Server.Models.Data;
+
+/// <summary>
+/// Represents a notification for a comment on an album.
+/// </summary>
+public class AlbumCommentNotification : BaseNotification
+{
+    /// <summary>
+    /// <see cref="AlbumComment.Id"/> of the comment that triggered this notification.
+    /// </summary>
+    [ForeignKey(nameof(Comment))]
+    public Guid CommentId { get; set; }
+
+    /// <summary>
+    /// The comment that triggered this notification.
+    /// </summary>
+    public AlbumComment? Comment { get; set; }
+}
