4668654bfad9851ec3d61e3de4cf7cfc7050f7fb
collenirwin <collenirwin@gmail.com>
2023-01-11 20:06:53 -0500

    add Friends and BlockedUsers props to ApplicationUser

--- Honk/Server/Models/Data/ApplicationUser.cs
+++ Honk/Server/Models/Data/ApplicationUser.cs
@@ -7,6 +7,16 @@ namespace Honk.Server.Models.Data;
 /// </summary>
 public class ApplicationUser : IdentityUser, ITimeTracked
 {
+    /// <summary>
+    /// All of the user's friends.
+    /// </summary>
+    public ICollection<ApplicationUser> Friends { get; set; } = null!;
+
+    /// <summary>
+    /// Users this user has blocked.
+    /// </summary>
+    public ICollection<ApplicationUser> BlockedUsers { get; set; } = null!;
+
     /// <inheritdoc />
     public DateTime CreatedOn { get; set; }
 
